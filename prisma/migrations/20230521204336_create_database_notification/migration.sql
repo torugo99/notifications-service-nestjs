9c485b6d3b2bc70852c588167c6851fab520b282
Victor Hugo <victormotabatista@gmail.com>
2023-05-21 18:12:39 -0300

    creating my prisma migration

---
+++ prisma/migrations/20230521204336_create_database_notification/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" TEXT NOT NULL,
+    "recipient_id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "read_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "notifications_recipient_id_idx" ON "notifications"("recipient_id");
