31f846c70a02aabf0f724749b830b2280a4f143c
Victor Hugo <victormotabatista@gmail.com>
2023-05-27 03:25:40 -0300

    mapper

--- src/application/entities/notification.entity.ts
+++ src/application/entities/notification.entity.ts
@@ -15,8 +15,11 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
@@ -52,7 +55,7 @@ export class Notification {
     this.props.readAt = new Date();
   }
   public unread() {
-    this.props.readAt = new Date();
+    this.props.readAt = null;
   }
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
--- src/infra/database/prisma/mappers/prisma-notification.mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification.mapper.ts
@@ -1,4 +1,5 @@
-import { Notification } from '@application/entities';
+import { Content, Notification } from '@application/entities';
+import { Notifications as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -7,8 +8,22 @@ export class PrismaNotificationMapper {
       category: notification.category,
       content: notification.content.value,
       recipientId: notification.recipientId,
+      canceledAt: notification.canceledAt,
       readAt: notification.readAt,
-      createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
