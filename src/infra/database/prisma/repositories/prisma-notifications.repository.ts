bfee4d5aa3c6cad732c89a3f2916a147b2d92364
Victor Hugo <victormotabatista@gmail.com>
2023-05-26 22:20:43 -0300

    fixing my repositories has to follow the contract

--- src/infra/database/prisma/repositories/prisma-notifications.repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications.repository.ts
@@ -2,21 +2,33 @@ import { NotificationsRepository } from '@application/repositories';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Notification } from '@application/entities';
+import { PrismaNotificationMapper } from '../mappers';
 
 @Injectable()
 export class PrismaNotificationRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const createNotification = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notifications.create({
-      data: {
-        id: notification.id,
-        category: notification.category,
-        content: notification.content.value,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        canceledAt: notification.createdAt,
-      },
+      data: createNotification,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  countManyByRecipientId(recipientId: string): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+
+  findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    throw new Error('Method not implemented.');
   }
 }
--- test/repositories/in-memory-notifications.repository.ts
+++ test/repositories/in-memory-notifications.repository.ts
@@ -9,4 +9,32 @@ export class InMemoryNotificationsRepository
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+    return notification;
+  }
+
+  countManyByRecipientId(recipientId: string): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+  findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    throw new Error('Method not implemented.');
+  }
 }
