414360a6bf19156cefeabae8e41772a8d7b7ac41
Victor Hugo <victormotabatista@gmail.com>
2023-05-27 00:17:40 -0300

    create count recipient use-case

---
+++ src/application/use-cases/count-recipient-notification.use-case.ts
@@ -0,0 +1,29 @@
+import { NotificationsRepository } from '@application/repositories';
+import { Injectable } from '@nestjs/common';
+
+interface CountRecipientNotificationRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CountRecipientNotificationRequest,
+  ): Promise<CountRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const count = await this.notificationsRepository.countManyByRecipientId(
+      recipientId,
+    );
+
+    return {
+      count,
+    };
+  }
+}
--- src/application/use-cases/index.ts
+++ src/application/use-cases/index.ts
@@ -1,2 +1,3 @@
 export * from './send-notification.use-case';
 export * from './cancel-notification.use-case';
+export * from './count-recipient-notification.use-case';
