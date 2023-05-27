358a733ea8f233e5bb70cf249832a3b2d11a38c4
Victor Hugo <victormotabatista@gmail.com>
2023-05-27 01:44:26 -0300

    update index.ts

--- src/application/use-cases/index.ts
+++ src/application/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from './send-notification.use-case';
 export * from './cancel-notification.use-case';
 export * from './count-recipient-notification.use-case';
+export * from './get-recipient-notification.use-case';
